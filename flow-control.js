a886fefea04d2ddffa2b2c96bd140f8df9c611dd
Asia Thomas <amthoms2@gmail.com>
2020-09-08 19:31:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function teenager(age) {
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    console.log("You are a teenager")
+    console.log("You are a teenager!")
   } else if (age < 13) {
     console.log ("You are a kid")
   } else
