d9f8831769d07f496661f10d9adee3330e9af75d
Asia Thomas <amthoms2@gmail.com>
2020-09-08 20:17:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,7 +24,7 @@ function ternaryTeenager(age) {
     return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
   }
 
-  ffunction switchAge(age) {
+  function switchAge(age) {
 switch(age) {
   case(13):
     return("You are a teenager");
