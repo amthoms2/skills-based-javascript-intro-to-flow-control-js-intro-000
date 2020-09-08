45abeb2372358cadee00052beba834c507c455b6
Asia Thomas <amthoms2@gmail.com>
2020-09-08 19:55:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,8 +21,7 @@ function ageChecker(age) {
     return "You are a grownup";}
 
 function ternaryTeenager(age) {
-  (age >= 13 && age <= 19)
-  return (ternaryTeenager ? "You are a teenager" : "You are not a teenager");
+  return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager");
   }
 
 function switchAge(age) {
